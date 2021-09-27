9ff14910fe7a6efb200fa31080a4be21110eaa07
atipatip <91176090+atipatip@users.noreply.github.com>
2021-09-27 10:52:05 +0700

    อัพโหลด โปรแจค flutter ชื่อ layout

---
+++ layout/lib/pages/home.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:layout/pages/detail.dart';
+
+class HomePage extends StatefulWidget {
+  //const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        title: Text("ความรู้เกี่ยวกับ..........."),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: FutureBuilder(builder: (context, snapshot) {
+          var data = json.decode(snapshot.data.toString());
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index){
+              return MyBox(data[index]['title'], data[index]['subtitle'], data[index]['image_url'], data[index]['detail']);
+
+            },
+            itemCount: data.length,);
+        },
+        future: DefaultAssetBundle.of(context).loadString('assets/data.json'),
+
+        )
+      )
+    );
+  }
+
+  Widget MyBox(String title, String subtitle, String image_url, String detail) {
+    var v1,v2,v3,v4;
+    v1 = title;
+    v2 = subtitle;
+    v3 = image_url;
+    v4 = detail;
+    
+    return Container(
+      margin: EdgeInsets.only(top:10),
+      padding: EdgeInsets.all(20),
+      //color: Colors.blue[50],
+      height: 200,
+      decoration: BoxDecoration(
+          // color: Colors.blue[50],
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: NetworkImage(image_url),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.25), BlendMode.darken),
+          )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.blue[50],
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            subtitle,
+            style: TextStyle(fontSize: 18, color: Colors.blue[50]),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextButton(
+              onPressed: () {
+                print("Next Page >>>");
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailPage(v1,v2,v3,v4)));
+              },
+              child: Text("อ่านต่อ >>>",style: TextStyle(fontSize: 18,color: Colors.redAccent),))
+        ],
+      ),
+    );
+  }
+
+  Widget MyBug() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "MyBug คืออะไร",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text(
+            "MyBugMyBugMyBugMyBugMyBugMyBug",
+            style: TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
